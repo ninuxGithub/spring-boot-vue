cbddc46475da0a3d3b3ec845dfebaa3dac5b0aed
ninuxGithub <1732768613@qq.com>
2017-08-08 11:13:16 +0800

    m

--- src/main/java/org/master/controller/PublicController.java
+++ src/main/java/org/master/controller/PublicController.java
@@ -11,7 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,7 +30,6 @@ public class PublicController {
 	@RequestMapping("/")
 	public ModelAndView index(ModelAndView model, HttpServletRequest request) {
 		List<User> users = userService.findAll();
-//		model.addObject("users", users);
 		request.getSession().setAttribute("users", users);
 		model.setViewName("index");
 		System.out.println(users);
@@ -37,6 +38,28 @@ public class PublicController {
 	@RequestMapping("/test")
 	public ModelAndView test(ModelAndView model, HttpServletRequest request) {
 		logger.info("test page...");
+		List<User> users = userService.findAll();
+		request.getSession().setAttribute("users", users);
+		model.setViewName("test");
+		return model;
+	}
+	
+	
+	@RequestMapping(value ="/input", method = RequestMethod.GET)
+	public ModelAndView addUser(ModelAndView model, HttpServletRequest request) {
+		logger.info("2 input...");
+		model.setViewName("input");
+		User user = new User();
+		user.setId(-1L);
+		user.setPassword("");
+		user.setUsername("");
+		request.getSession().setAttribute("user", user);
+		return model;
+	}
+	@RequestMapping(value ="/inputUser", method = RequestMethod.POST)
+	public ModelAndView inputUser(@ModelAttribute User user, ModelAndView model, HttpServletRequest request) {
+		logger.info("添加的用户为：{} ", user.toString());
+		
 		List<User> users = userService.findAll();
 		request.getSession().setAttribute("users", users);
 		model.setViewName("test");
